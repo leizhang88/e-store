378bc275865a3346b1455a1b79deb854a2c7e734
leiz <callmelei@outlook.com>
2022-01-16 19:30:54 -0500

    Add persistence components that implements dropdown list of province

---
+++ src/main/java/com/cy/store/mapper/DistrictMapper.java
@@ -0,0 +1,14 @@
+package com.cy.store.mapper;
+
+import com.cy.store.entity.District;
+
+import java.util.List;
+
+public interface DistrictMapper {
+    /**
+     * Find districts by parent code
+     * @param parent
+     * @return
+     */
+    List<District> findByParent(String parent);
+}
