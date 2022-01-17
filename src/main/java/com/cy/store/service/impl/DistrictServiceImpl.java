c7299b6279b9649514ff6efc00f924df0487b54c
leiz <callmelei@outlook.com>
2022-01-16 20:11:25 -0500

    Add service and controller components that implement dropdown list of districts

---
+++ src/main/java/com/cy/store/service/impl/DistrictServiceImpl.java
@@ -0,0 +1,27 @@
+package com.cy.store.service.impl;
+
+import com.cy.store.entity.District;
+import com.cy.store.mapper.DistrictMapper;
+import com.cy.store.service.IDistrictService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DistrictServiceImpl implements IDistrictService {
+    @Autowired
+    private DistrictMapper districtMapper;
+
+    @Override
+    public List<District> getByParent(String parent) {
+        List<District> list =  districtMapper.findByParent(parent);
+
+        // Set unused properties of found object to null
+        for(District d : list) {
+            d.setId(null);
+            d.setParent(null);
+        }
+        return list;
+    }
+}
