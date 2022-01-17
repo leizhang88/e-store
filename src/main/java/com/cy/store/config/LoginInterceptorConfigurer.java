a2e451748b2e87ebff511b7ceeff007f4024109a
leiz <callmelei@outlook.com>
2022-01-16 20:05:17 -0500

    Add interceptor configure files

---
+++ src/main/java/com/cy/store/config/LoginInterceptorConfigurer.java
@@ -0,0 +1,39 @@
+package com.cy.store.config;
+
+import com.cy.store.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class LoginInterceptorConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // Create an interceptor object
+        HandlerInterceptor interceptor = new LoginInterceptor();
+
+        // Create a white list for interceptor
+        List<String> patterns = new ArrayList<>();
+        patterns.add("/bootstrap3/**");
+        patterns.add("/css/**");
+        patterns.add("/images/**");
+        patterns.add("/js/**");
+        patterns.add("/web/register.html");
+        patterns.add("/web/login.html");
+        patterns.add("/web/index.html");
+        patterns.add("/web/product.html");
+        patterns.add("/users/reg");
+        patterns.add("/users/login");
+        patterns.add("/districts/**");
+
+        // Register the interceptor
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(patterns);
+    }
+}
